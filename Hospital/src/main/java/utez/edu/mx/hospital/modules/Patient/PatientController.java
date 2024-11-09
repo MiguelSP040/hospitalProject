5764ebd48875858afb4e76be6632abcac71bfa59
RatQuesadilla <20233tn175@utez.edu.mx>
2024-11-08 21:05:10 -0600

    Modulo Pacientes

--- Hospital/src/main/java/utez/edu/mx/hospital/modules/Patient/PatientController.java
+++ Hospital/src/main/java/utez/edu/mx/hospital/modules/Patient/PatientController.java
@@ -1,9 +1,37 @@
 package utez.edu.mx.hospital.modules.Patient;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/patient")
 public class PatientController {
+    @Autowired
+    private PatientService patientService;
+
+    @GetMapping("")
+    public ResponseEntity<?> getAllPatients() {
+        return patientService.findAllPatients();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getPatientById(@PathVariable long id) {
+        return patientService.findPatientById(id);
+    }
+
+    @PostMapping("")
+    public ResponseEntity<?> addPatient(@RequestBody Patient patient) {
+        return patientService.savePatient(patient);
+    }
+
+    @PutMapping("")
+    public ResponseEntity<?> updatePatient(@RequestBody Patient patient) {
+        return patientService.updatePatient(patient);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> changePatientDischarge(@PathVariable long id) {
+        return patientService.changePatientDischarge(id);
+    }
 }
