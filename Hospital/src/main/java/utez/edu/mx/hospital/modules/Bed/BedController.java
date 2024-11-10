d95c31a6e40cac308efce38c86b1d859f8bf2f46
Aqquinou <20233tn183@utez.edu.mx>
2024-11-09 23:12:54 -0600

    estructura

--- Hospital/src/main/java/utez/edu/mx/hospital/modules/Bed/BedController.java
+++ Hospital/src/main/java/utez/edu/mx/hospital/modules/Bed/BedController.java
@@ -2,13 +2,57 @@ package utez.edu.mx.hospital.modules.Bed;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import utez.edu.mx.hospital.modules.Floor.Floor;
+import utez.edu.mx.hospital.modules.Floor.FloorService;
+import utez.edu.mx.hospital.modules.User.User;
+import utez.edu.mx.hospital.modules.User.UserService;
 
 @RestController
 @RequestMapping("/api/bed")
 public class BedController {
+    @Autowired
+    private BedService bedService;
 
+    //Traer todas las camas
+    @GetMapping("")
+    public ResponseEntity<?> findAll(){
+        return bedService.findAll();
+    }
+
+    //traer por el id de cama
+    @GetMapping("/{idBed}")
+    public ResponseEntity<?> findById(@PathVariable("idBed") long idBed){
+        return bedService.findById(idBed);
+    }
+
+    //guardar cama
+    @PostMapping("")
+    public ResponseEntity<?> save(@RequestBody Bed bed){
+        return bedService.save(bed);
+    }
+
+    //actualizar cama
+    @PutMapping("")
+    public ResponseEntity<?> update(@RequestBody Bed bed){
+        return bedService.update(bed);
+    }
+
+    //Encontrar camas por piso
+    @GetMapping("/bedOnFloor")
+    public ResponseEntity<?> findBedOnFloor(@RequestBody Floor floor){
+        return bedService.findByFloor(floor);
+    }
+
+    //Encontrar camas disponibles por piso
+    @GetMapping("/{idFloor}")
+    public ResponseEntity<?> findAvailableByFloor(@PathVariable("idFloor") long idFloor){
+        return bedService.findAvailableBedsByFloor(idFloor);
+    }
+
+    //Encontrar camas ocupadas por un paciente
+    @GetMapping("/{idPatient}")
+    public ResponseEntity<?> findAvailableByPatient(@PathVariable("idPatient") long idPatient){
+        return bedService.findOccupiedBedsByPatient(idPatient);
+    }
 }
